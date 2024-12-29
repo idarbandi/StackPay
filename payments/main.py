f945c8bb3fb4ee0e93b7083c4a94873afbe122bb
idarbandi <darbandidr99@gmail.com>
2024-12-29 11:36:12 +0330

    payments service more functionality

--- payments/main.py
+++ payments/main.py
@@ -1,10 +1,13 @@
 import logging
+import time
 
+import requests
 from fastapi import FastAPI, HTTPException
+from fastapi.background import BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from redis_om import HashModel, get_redis_connection
-from starlette.requests import request
+from starlette.requests import Request
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -41,10 +44,37 @@ class Order(BaseModel):
     class Meta:
         database = redis
 
-    @app.post('/orders')
-    async def create(requests: Request):
-        body = await request.json()
 
-        req = request.get('http://localhost:8000/products/%s' % body['id'])
-        
-        return req.json()
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
+
+@app.post('/orders')
+async def create(request: Request, background_tasks=BackgroundTasks):
+    body = await request.json()
+
+    req = request.get('http://localhost:8000/products/%s' % body['id'])
+    if not req:
+        return {
+            '404 Not Found'
+        }
+    product = req.json()
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    background_tasks.add_task(order_created, order)
+    return order
+
+
+def order_created(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
